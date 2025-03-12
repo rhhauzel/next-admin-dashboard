316e272969667178bddea8e5884446cb64523fdc
Raphael Hauzel <rhhauzel@gmail.com>
2025-03-12 08:14:12 +0530

    05-load-status-boxes-from-database

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,5 +1,7 @@
+import StatCardsWrapper from "@/components/shared/dashboard/stat-cards-wrapper";
 import { lusitana } from "@/components/shared/fonts";
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/components/shared/skeletons";
+import { Suspense } from "react";
 
 export default async function Page(){
     return (
@@ -8,7 +10,9 @@ export default async function Page(){
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <CardsSkeleton />
+                <Suspense fallback={<CardsSkeleton />}>
+                    <StatCardsWrapper />
+                </Suspense>
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChartSkeleton />
