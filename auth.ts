fbd499684145b6db13515a55539078d9ffc5c855
Raphael Hauzel <rhhauzel@gmail.com>
2025-03-12 10:29:42 +0530

    08-authenticate-user-from-database

--- auth.ts
+++ auth.ts
@@ -1,23 +1,27 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
-import { users } from "./lib/placeholder-data";
 import { compare } from "bcryptjs";
+import { z } from 'zod'
+import { getUser } from "./lib/actions/user.actions";
 
 export const { auth, signIn, signOut} = NextAuth({
     ...authConfig,
     providers: [
         Credentials({
             async authorize(credentials){
-                const user = users.find((x) => x.email === credentials.email)
-            
-                //console.log(user)
-                if(!user) return null
-                const passwordMatch = await compare(credentials.password as string, user.password)
-                if(passwordMatch){
-                    //console.log("correct")
-                    return user
+                const parsedCredentials = z
+                    .object({email: z.string().email(), password: z.string().min(6)})
+                    .safeParse(credentials)
+                
+                if(parsedCredentials.success){
+                    const {email, password} = parsedCredentials.data
+                    const user = await getUser(email)
+                    if(!user) return null
+                    const passwordsMatch = await compare(password, user.password)
+                    if(passwordsMatch) return user
                 }
+                
                 console.log('Invalid credentials')
                 return null
             },
--- lib/placeholder-data.ts
+++ lib/placeholder-data.ts
@@ -1,4 +1,4 @@
-import { hashSync } from "bcryptjs"
+import { hashSync } from 'bcryptjs'
 
 const users = [
   {
