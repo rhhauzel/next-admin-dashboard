04be5f41703321128342d04d8906455cd6b59e51
Raphael Hauzel <rhhauzel@gmail.com>
2025-03-13 08:48:22 +0530

    10-create-or-update-invoice

--- lib/actions/invoice.actions.ts
+++ lib/actions/invoice.actions.ts
@@ -6,6 +6,9 @@ import { count, sql, eq, desc, ilike, or } from "drizzle-orm"
 import { formatCurrency } from "../utils"
 import { revalidatePath } from "next/cache"
 import { ITEMS_PER_PAGE } from "../constants"
+import { z } from "zod"
+import { redirect } from "next/navigation"
+import { InvoiceForm } from "@/types"
 
 export async function fetchCarData() {
     try {
@@ -145,3 +148,126 @@ export async function fetchInvoicesPages(query: string){
         throw new Error('Failed to fetch total number of invoices.')
     }
 }
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, {message: 'Please enter an amount greater than 0.'}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select a status.',
+    }),
+    date: z.string(),
+})
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+const UpdateInvoice = FormSchema.omit({ date: true, id: true })
+
+export type State = {
+    errors?: {
+        customerId?: string[]
+        amount?: string[]
+        status?: string[]
+    }
+    message?: string | null
+}
+
+export async function createInvoice(prevState: State, formData: FormData){
+    const validateFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: Number(formData.get('amount')),
+        status: formData.get('status'),
+    })
+
+    if(!validateFields.success){
+        return {
+            errors: validateFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        }
+    }
+
+    const { customerId, amount, status } = validateFields.data
+    const amountInCents = amount * 100
+    const date = new Date().toISOString().split('T')[0]
+
+    try {
+        await db.insert(invoices).values({
+            customer_id: customerId,
+            amount: amountInCents,
+            status,
+            date,
+        })
+    }catch(error){
+        return {
+            message: `Database Error: Failed to Create Invoice. ${error}`
+        }
+    }
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData
+){
+    const validateFields = UpdateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: Number(formData.get('amount')),
+        status: formData.get('status'),
+    })
+
+    if(!validateFields.success){
+        return {
+            errors: validateFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        }
+    }
+
+    const { customerId, amount, status} = validateFields.data
+    const amountInCents = amount * 100
+
+    try {
+        await db
+            .update(invoices)
+            .set({
+                customer_id: customerId,
+                amount: amountInCents,
+                status,
+            })
+            .where(eq(invoices.id, id))
+    }catch(error){
+        return { message: `Database Error: Faile to update Invoice. ${error}`}
+    }
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+export async function fetchInvoiceById(id: string) {
+    try {
+      const data = await db
+        .select({
+          id: invoices.id,
+          customer_id: invoices.customer_id,
+          amount: invoices.amount,
+          status: invoices.status,
+          date: invoices.date,
+        })
+        .from(invoices)
+        .where(eq(invoices.id, id))
+  
+      const invoice = data.map((invoice) => ({
+        ...invoice,
+        // Convert amount from cents to dollars
+        status: invoice.status === 'paid' ? 'paid' : 'pending',
+        amount: invoice.amount / 100,
+      }))
+  
+      return invoice[0] as InvoiceForm
+    } catch (error) {
+      console.error('Database Error:', error)
+      throw new Error('Failed to fetch invoice.')
+    }
+  }
